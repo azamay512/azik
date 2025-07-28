9d2c50475a9e4ac6d4026d951eeba81be4ec289f
azamay512 <azamay512@gmail.com>
2025-07-28 14:07:15 +0500

    Add files via upload

---
+++ azamat_22/js25.js
@@ -0,0 +1,22 @@
+// let mevalar = ["Olma" , "shaptoli" , "banana"]
+// mevalar.forEach((move , index) => {
+//     console.log(index , move);
+// });
+
+// let arry = ["Azamat" , "Ulg'ugbek" , "Javohir"]
+// arry.splice(1, 2)
+// alert(arry)
+
+// let arry = ["Azamat" , "Ulg'ugbek" , "Javohir"]
+// arry.splice(1, 2 , "Ulug'bek")
+// alert(arry)
+
+// let mevalar = ["Olma" , "shaptoli" , "banana"]
+// let b = mevalar.splice(0 , 1)
+// alert(`${b} .........`)
+
+// let arry = ["Azamat" , "Ulg'ugbek" , "Javohir"]
+//  arry.forEach((  item , move , index ,) => {
+//      alert(`${item} ni yandex dagi urni ${move}  ni yandex dagi urni ${index}  ni yandex dagi urni`)
+//   });
+ 
